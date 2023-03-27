28939db7a6691b959a3a159ad9a297cb9beca80e
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-03-27 08:06:25 +0300

    modified DN and PE srcipts

--- Analytics-in-BigQuery/v2-Scripts-for-Analysis/v2-Data-Analysis/Scripts-for-Data-Validation/Payment-Entry-Summary.sql
+++ Analytics-in-BigQuery/v2-Scripts-for-Analysis/v2-Data-Analysis/Scripts-for-Data-Validation/Payment-Entry-Summary.sql
@@ -5,8 +5,9 @@ payment_entry_with_index as (
                               FROM `kyosk-prod.erp_reports.payment_entry` 
                               WHERE payment_type = "Receive"
                               --and DATE(creation) between '2023-02-06' and '2023-02-19'
-                              and name in ('ACC-PAY-2022-1555336')
+                              --and name in ('ACC-PAY-2022-1555336')
+                              and posting_date >= '2023-03-20' --and '2023-03-26'
                               ),
-payment_entry_summary as (select distinct name from payment_entry_with_index where index = 1)
+payment_entry_summary as (select distinct name, paid_amount from payment_entry_with_index where index = 1)
 
 select * from payment_entry_summary 
