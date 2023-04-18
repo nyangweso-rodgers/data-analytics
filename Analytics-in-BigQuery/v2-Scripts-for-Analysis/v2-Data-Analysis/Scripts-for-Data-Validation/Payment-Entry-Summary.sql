a0f306a29778f672fa2a232ab7f35d96aaeef712
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-04-18 11:11:31 +0300

    modiifed sales invoice and payment entry summary scripts

--- Analytics-in-BigQuery/v2-Scripts-for-Analysis/v2-Data-Analysis/Scripts-for-Data-Validation/Payment-Entry-Summary.sql
+++ Analytics-in-BigQuery/v2-Scripts-for-Analysis/v2-Data-Analysis/Scripts-for-Data-Validation/Payment-Entry-Summary.sql
@@ -3,11 +3,11 @@ with
 payment_entry_with_index as (
                               SELECT *, row_number()over(partition by name order by modified desc) as index
                               FROM `kyosk-prod.erp_reports.payment_entry` 
-                              --WHERE payment_type = "Receive"
+                              WHERE payment_type = "Receive"
                               --and company  'KYOSK DIGITAL SERVICES LTD (KE)'
-                              --and DATE(creation) between '2023-02-06' and '2023-02-19'
                               --and name in ('ACC-PAY-2023-192824', 'PAY-KARA-2023-15588')
-                              where posting_date between '2023-01-01' and '2023-01-31'
+                              --and posting_date between '2023-04-01' and '2023-04-17'
+                              --and posting_date in ('2023-03-04', '2023-03-05', '2023-03-11', '2023-03-14')
                               ),
 payment_entry_summary as (
                           select distinct posting_date,
@@ -16,9 +16,8 @@ payment_entry_summary as (
                           group by 1 order by 1
                           ),                              
 payment_entry_lists as (
-                          select distinct name
-                          --count(distinct name)
+                          select distinct name,kyosk_sales_order, paid_amount
                           from payment_entry_with_index where index = 1
                           )
 
-select * from payment_entry_summary 
+select * from payment_entry_lists 
