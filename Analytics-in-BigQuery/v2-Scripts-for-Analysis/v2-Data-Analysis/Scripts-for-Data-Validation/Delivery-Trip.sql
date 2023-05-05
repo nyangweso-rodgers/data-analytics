7c104b36b6d7a72f4fcefde8c33061543fd3b6af
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-05-05 19:27:29 +0300

    edit to delivery trip

--- Analytics-in-BigQuery/v2-Scripts-for-Analysis/v2-Data-Analysis/Scripts-for-Data-Validation/Delivery-Trip.sql
+++ Analytics-in-BigQuery/v2-Scripts-for-Analysis/v2-Data-Analysis/Scripts-for-Data-Validation/Delivery-Trip.sql
@@ -1,12 +1,18 @@
 ---------------------------- QA - Delivery Trip ------------------------
-with delivery_trip_with_index as (
+with 
+delivery_trip_with_index as (
                                     SELECT *, 
                                     row_number()over(partition by name order by modified desc) as index
                                     FROM `kyosk-prod.erp_reports.delivery_trip`
-                                    --where date(creation) between '2022-12-01' and '2023-01-28'
+                                    where date(creation) between '2023-01-01' and '2023-05-04'
                                     ),
-delivery_trip_summary as (select distinct date(creation) as creation_date, count(distinct name) as count from  delivery_trip_with_index where index = 1 group by 1 order by 1 desc),
+delivery_trip_summary as (
+                          select distinct date(creation) as creation_date, 
+                          count(distinct name) as count 
+                          from  delivery_trip_with_index where index = 1 
+                          group by 1 order by 1 desc
+                          ),
 delivery_trip_lists as (select distinct date(creation) as creation_date, name, workflow_state from  delivery_trip_with_index where index = 1 order by 1)
 
-select distinct date(dt.creation),dt.name, workflow_state, count(distinct delivery_note)
-from delivery_trip_with_index dt, unnest(delivery_stops) ds
+select *
+from delivery_trip_summary
