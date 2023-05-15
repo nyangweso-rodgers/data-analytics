6e85b44b8e111f2e1bbf5b166f751c2ea0f6f345
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-05-15 20:09:37 +0300

    new versions for payment entry and sales invoice

--- Analytics-in-BigQuery/v2-Scripts-for-Analysis/v2-Data-Analysis/Scripts-for-Data-Validation/Payment-Entry-Summary.sql
+++ Analytics-in-BigQuery/v2-Scripts-for-Analysis/v2-Data-Analysis/Scripts-for-Data-Validation/Payment-Entry-Summary.sql
@@ -5,9 +5,7 @@ payment_entry_with_index as (
                               FROM `kyosk-prod.erp_reports.payment_entry` 
                               WHERE payment_type = "Receive"
                               --and company  'KYOSK DIGITAL SERVICES LTD (KE)'
-                              --and name in ('ACC-PAY-2023-192824', 'PAY-KARA-2023-15588')
-                              --and posting_date between '2023-04-01' and '2023-04-17'
-                              --and posting_date in ('2023-03-04', '2023-03-05', '2023-03-11', '2023-03-14')
+                              and posting_date between '2023-05-01' and '2023-05-14'
                               ),
 payment_entry_summary as (
                           select distinct posting_date,
@@ -16,7 +14,8 @@ payment_entry_summary as (
                           group by 1 order by 1
                           ),                              
 payment_entry_lists as (
-                          select distinct name,kyosk_sales_order, paid_amount
+                          select distinct name, 
+                          paid_amount
                           from payment_entry_with_index where index = 1
                           )
 
--- Analytics-in-BigQuery/v2-Scripts-for-Analysis/v2-Data-Analysis/Scripts-for-Data-Validation/Sales-Invoice-Summary.sql
+++ Analytics-in-BigQuery/v2-Scripts-for-Analysis/v2-Data-Analysis/Scripts-for-Data-Validation/Sales-Invoice-Summary.sql
@@ -4,10 +4,8 @@ sales_invoice_with_index as (
                             SELECT *, 
                             row_number()over(partition by name order by modified desc) as index 
                             FROM `kyosk-prod.erp_reports.sales_invoice` 
-                            --where posting_date between '2023-02-01' and '2023-02-28'
-                            --and posting_date in ('2023-02-01', '2023-02-02', '2023-02-09')
-                            where company = 'KYOSK DIGITAL SERVICES LTD (KE)'
-                            --and docstatus = 0
+                            where posting_date between '2023-05-01' and '2023-05-14'
+                            --where company = 'KYOSK DIGITAL SERVICES LTD (KE)'
                             ),
 sales_invoice_summary as (
                           select distinct si.posting_date, 
@@ -18,8 +16,8 @@ sales_invoice_summary as (
 
 sales_invoice_lists as (
                         select distinct si.name,
+                        grand_total
                         from sales_invoice_with_index si where index = 1
                         )
 
-select distinct po_no, min(posting_date) from sales_invoice_with_index where index = 1
-group by 1 order by 2
+select * from sales_invoice_lists 
