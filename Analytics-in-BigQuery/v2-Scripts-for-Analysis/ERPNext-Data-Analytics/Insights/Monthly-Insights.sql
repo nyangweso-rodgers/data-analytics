e0286bc5003ecb94aae7f54b01769c76fb4d127b
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-07-03 14:28:13 +0300

    ++ monthly insights query

--- Analytics-in-BigQuery/v2-Scripts-for-Analysis/ERPNext-Data-Analytics/Insights/Monthly-Insights.sql
+++ Analytics-in-BigQuery/v2-Scripts-for-Analysis/ERPNext-Data-Analytics/Insights/Monthly-Insights.sql
@@ -1,16 +1,15 @@
----------------------------------- Weekly Insights -----------------------------------------
+------------------------- Monthly Insights ------------------------
 with
-
 paid_and_delivered_dns as (
                           SELECT  *
                           FROM `kyosk-prod.erp_scheduled_queries.erp_paid_and_delivered_dns` 
                           where territory not in ('Test KE Territory', "Test UG Territory")
                           ),
-
-current_week as (
-                  select distinct date_trunc(posting_date, week(monday)) as week,
+previous_month as (
+                  select distinct date_trunc(posting_date, month) as month,
                   company,
                   count(distinct territory) as territories,
+                  --count(distinct sales_partner),
                   count(distinct customer) as customers,
                   count(distinct name) as dns,
                   count(distinct name) / count(distinct customer) as avg_delivery_freq,
@@ -19,12 +18,12 @@ current_week as (
                   sum(grand_total) / count(distinct customer) as avg_customer_gmv,
                   sum(grand_total) / count(distinct territory) as gmv_per_territory
                   from paid_and_delivered_dns
-                  WHERE date_trunc(posting_date, week(monday)) = '2023-05-22'
+                  WHERE date_trunc(posting_date, month) = '2023-06-01'
                   group by 1,2
                   order by 1,2
                   ),
-previous_week as (
-                  select distinct date_trunc(posting_date, week(monday)) as week,
+prior_month as (
+                  select distinct date_trunc(posting_date, month) as month,
                   company,
                   count(distinct territory) as territories,
                   count(distinct customer) as customers,
@@ -35,8 +34,8 @@ previous_week as (
                   sum(grand_total) / count(distinct customer) as avg_customer_gmv,
                   sum(grand_total) / count(distinct territory) as gmv_per_territory
                   from paid_and_delivered_dns
-                  WHERE date_trunc(posting_date, week(monday)) = '2023-05-15'
+                  WHERE date_trunc(posting_date, month) = '2023-05-01'
                   group by 1,2
                   order by 1,2
                   )
-select * from previous_week
+select * from previous_month
