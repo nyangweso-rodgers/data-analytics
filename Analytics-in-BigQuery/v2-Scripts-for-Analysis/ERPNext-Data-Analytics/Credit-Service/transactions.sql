b34438bc2b29f13f497e7e4694fcf11c6dfe7d6c
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-10-05 19:38:11 +0300

    deleted credit-service folder

--- Analytics-in-BigQuery/v2-Scripts-for-Analysis/ERPNext-Data-Analytics/Credit-Service/transactions.sql
+++
@@ -1,9 +0,0 @@
------------------------------ Transactions ------------------
-with
-transactions as (
-                  SELECT *, row_number()over(partition by ID order by  LAST_MODIFIED_DATE desc) as index 
-                  FROM `kyosk-prod.erp_reports.etl_credit_service_transactions_prod` 
-                  WHERE DATE(creation) <= "2023-03-14"
-                  )
-
-select count(*) from transactions where index =1
