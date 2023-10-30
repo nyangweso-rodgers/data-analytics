5ec78edd6bd81e18851f7be4767da6ec2896c17a
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-10-30 20:12:19 +0300

    added erp-so.sql

--- Analytics-in-BigQuery/v2-Scripts-for-Analysis/KARURU-Analytics/ERPNext-Data-Validation-Scripts/erp-so.sql
+++ Analytics-in-BigQuery/v2-Scripts-for-Analysis/KARURU-Analytics/ERPNext-Data-Validation-Scripts/erp-so.sql
@@ -5,22 +5,17 @@ erp_so as (
             SELECT *,
             row_number()over(partition by name order by modified desc) as index
             FROM `kyosk-prod.erp_reports.sales_order` 
-            where date(creation) between '2023-10-01' and  '2023-10-25'
+            where date(creation) between '2022-02-01' and  '2023-10-25'
             --where date(creation) = '2023-10-25'
-            --and territory not in ("Kyosk TZ HQ", "Kampala","Uganda","DKasarani","Kyosk HQ", "Kenya") 
+            and territory not in ('Test NG Territory', 'Kyosk TZ HQ', 'Test TZ Territory', 'Kyosk HQ','DKasarani', 'Test KE Territory', 'Test UG Territory')
             --and workflow_state not in ('INITIATED', 'USER_CANCELLED', 'EXPIRED')
             --and company = 'KYOSK DIGITAL SERVICES LIMITED (TZ)'
             ),
 so_report as (
-              select distinct status,
-              name
+              select DISTINCT name,
+              workflow_state,
               from erp_so so
               where index = 1
-              --and status = 'Draft'
-              --and name in ('SAL-ORD-EMBRGNL', 'SAL-ORD-KISWU5H')
+              and name in ('SAL-ORD-EMBRGNL', 'SAL-ORD-KISWU5H')
               )
-select distinct status,
-count(distinct name) as names
-from so_report
-group by 1
-order by 2 desc
+select * from so_report
