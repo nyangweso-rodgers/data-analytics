b9ea8f1d14215d0fbdc80bf6439530a35bcb5134
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-11-06 22:32:28 +0300

    modified dn script

--- Analytics-in-BigQuery/v2-Scripts-for-Analysis/KARURU-Analytics/Scripts-for-Data-Validation/karuru-specific-scripts/karuru-dns.sql
+++ Analytics-in-BigQuery/v2-Scripts-for-Analysis/KARURU-Analytics/Scripts-for-Data-Validation/karuru-specific-scripts/karuru-dns.sql
@@ -9,18 +9,21 @@ karuru_dns as (
                 and date(created_at) > '2023-07-01'
                 and is_pre_karuru = false
                 ),
-karuru_dns_items as (
-                    select distinct date(created_at) as created_at,
-                    country_code,
-                    id,
-                    code,
-                    dn.status,
-                    dni.product_bundle_id,
-                    from karuru_dns dn, unnest(order_items) dni
-                    where index = 1
-                    --and country_code = 'TZ'
-                    --AND dn.status IN ('PAID', 'DELIVERED', 'CASH_COLLECTED')
-                    --and dni.status = 'ITEM_FULFILLED'
-                    )
-select distinct created_at,country_code, id, code
-from karuru_dns_items
+dns_items as (
+              select distinct date(created_at) as created_at,
+              country_code,
+              id,
+              code,
+              dn.status,
+              dni.product_bundle_id,
+              dni.item_group_id
+              from karuru_dns dn, unnest(order_items) dni
+              where index = 1
+              --and country_code = 'TZ'
+              --AND dn.status IN ('PAID', 'DELIVERED', 'CASH_COLLECTED')
+              --and dni.status = 'ITEM_FULFILLED'
+              )
+select distinct product_bundle_id
+from dns_items
+where item_group_id is null
+--order by 1 desc, code, product_bundle_id
