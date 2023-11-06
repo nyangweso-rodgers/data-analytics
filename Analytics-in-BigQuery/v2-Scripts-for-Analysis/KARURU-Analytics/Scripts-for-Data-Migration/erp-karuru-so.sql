7c7432bc995bc23a6693f79df4ed760fc951f4b1
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-11-06 22:31:39 +0300

    modified so script for data migration

--- Analytics-in-BigQuery/v2-Scripts-for-Analysis/KARURU-Analytics/Scripts-for-Data-Migration/erp-karuru-so.sql
+++ Analytics-in-BigQuery/v2-Scripts-for-Analysis/KARURU-Analytics/Scripts-for-Data-Migration/erp-karuru-so.sql
@@ -5,13 +5,13 @@ erp_so as (
             SELECT *,
             row_number()over(partition by name order by modified desc) as index
             FROM `kyosk-prod.erp_reports.sales_order` 
-            where date(creation) between '2022-02-01' and '2023-10-29'
+            where date(creation) between '2022-02-01' and '2023-11-01'
             and territory not in ('Test NG Territory', 'Kyosk TZ HQ', 'Test TZ Territory', 'Kyosk HQ','DKasarani', 'Test KE Territory', 'Test UG Territory')
             ),
 erp_order_items as (
                     select distinct so.name,
                     so.workflow_state,
-                    --date(creation) as creation,
+                    date(creation) as creation,
                     --sales_partner
                     from erp_so so
                     where index = 1
@@ -20,13 +20,13 @@ karuru_so as (
               SELECT *,
               row_number()over(partition by name  order by last_modified_date desc) as index
               FROM `kyosk-prod.karuru_reports.sales_order` so
-              WHERE date(created_date) between '2022-02-01' and '2023-09-20'
+              WHERE date(created_date) between '2022-02-01' and '2023-11-01'
               and territory_id not in ('Test NG Territory', 'Kyosk TZ HQ', 'Test TZ Territory', 'Kyosk HQ','DKasarani', 'Test KE Territory', 'Test UG Territory')
               and is_pre_karuru = true
               ),
 karuru_order_items as (
                         select distinct so.name,
-                        --date(created_date) as created_date,
+                        date(created_date) as created_date,
                         so.order_status,
                         --market_developer_name
                         from karuru_so so
@@ -34,14 +34,15 @@ karuru_order_items as (
                         ),
 mashup as (
             select e.*,
-            --k.created_date,
+            k.created_date,
             --k.market_developer_name,
-            --e.creation = k.created_date as date_check,
+            e.creation = k.created_date as check_date,
             --e.sales_partner = k.market_developer_name as md_check
             k.order_status
             from erp_order_items e
             left join karuru_order_items k on e.name = k.name
             )
-select distinct workflow_state, order_status
+select *
 from mashup
+where check_date = false
 order by 1,2
