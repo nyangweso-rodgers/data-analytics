caed6ee5ef0a7f0f8009ec7b52ed509272486f6d
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-11-09 23:19:48 +0300

    modified the dns script

--- Analytics-in-BigQuery/v2-Scripts-for-Analysis/KARURU-Analytics/Scripts-for-Data-Validation/karuru-specific-scripts/karuru-dns.sql
+++ Analytics-in-BigQuery/v2-Scripts-for-Analysis/KARURU-Analytics/Scripts-for-Data-Validation/karuru-specific-scripts/karuru-dns.sql
@@ -9,22 +9,18 @@ karuru_dns as (
                 and date(created_at) > '2023-08-05'
                 and is_pre_karuru = false
                 ),
-dns_items as (
+dns_list as (
               select distinct date(created_at) as created_at,
               country_code,
               id,
-              code,
-              dn.sale_order_id,
+              --code,
+              --dn.sale_order_id,
               dn.status,
-              dni.product_bundle_id,
-              dni.item_group_id
-              from karuru_dns dn, unnest(order_items) dni
+              from karuru_dns dn
               where index = 1
               --and country_code = 'TZ'
-              --AND dn.status IN ('PAID', 'DELIVERED', 'CASH_COLLECTED')
+              AND dn.status not IN ('PAID', 'DELIVERED')
               --and dni.status = 'ITEM_FULFILLED'
               )
-select distinct product_bundle_id
-from dns_items
-where item_group_id is null
---order by 1 desc, code, product_bundle_id
+select *
+from dns_list
