0bc340f4ad7c135e157e26b8b4b20d2c882fc329
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-11-09 22:17:13 +0300

    modified the date in dns and dns-items

--- Analytics-in-BigQuery/v2-Scripts-for-Analysis/KARURU-Analytics/ERPNext-Data-Validation-Scripts/erp-dns-items.sql
+++ Analytics-in-BigQuery/v2-Scripts-for-Analysis/KARURU-Analytics/ERPNext-Data-Validation-Scripts/erp-dns-items.sql
@@ -1,34 +1,37 @@
------------------------ ERPNext ------------
+----------------------- ERP ------------
 ----------------------DNs Items --------------
 with 
 erp_dns as (
             SELECT *, 
             row_number()over(partition by name order by modified desc) as index 
             FROM `kyosk-prod.erp_reports.delivery_note` 
-            where territory not in ('Test NG Territory', 'Kyosk TZ HQ', 'Test TZ Territory', 'Kyosk HQ','DKasarani', 'Test KE Territory', 'Test UG Territory')
-            and date(creation) = '2023-07-22'
-            --and company = 'KYOSK DIGITAL SERVICES LTD (KE)'
-            --'KYOSK DIGITAL SERVICES LIMITED (UG)'
-            --'KYOSK DIGITAL SOLUTIONS NIGERIA LIMITED'
+            where date(creation) between '2022-02-01' and '2023-11-08'
+            --where territory not in ('Test NG Territory', 'Kyosk TZ HQ', 'Test TZ Territory', 'Kyosk HQ','DKasarani', 'Test KE Territory', 'Test UG Territory')
             ),
 erp_items as (
-                select distinct company,
-                dn.territory,
+                select distinct date(dn.creation) as creation,
+                company,
+                --dn.territory,
                 posting_date,
                 --customer,
                 dn.name,
-                dn.workflow_state,
+                --dn.workflow_state,
                 dni.item_code,
                 dni.uom,
                 sum(dni.base_amount) as base_amount
                 from erp_dns dn, unnest(items) dni
                 where index = 1
-                --and workflow_state in ('PAID', 'DELIVERED')
-                --and posting_date between '2023-08-01' and '2023-08-31'
-                and company = 'KYOSK DIGITAL SERVICES LIMITED (TZ)'
-                group by 1,2,3,4,5,6,7
+                and workflow_state in ('PAID', 'DELIVERED')
+                group by 1,2,3,4,5,6
                 order by 3,4,6
-                )
+                ),
+monthly_agg as (
+                  select distinct date_trunc(creation, month) as creation_month,
+                  company,
+                  sum(base_amount) as base_amount
+                  from erp_items
+                  group by 1,2
+                  order by 2, 1
+                  )
 select *
 from erp_items
-where name = "DN-MWEN-IMMT"
--- Analytics-in-BigQuery/v2-Scripts-for-Analysis/KARURU-Analytics/ERPNext-Data-Validation-Scripts/erp-dns.sql
+++ Analytics-in-BigQuery/v2-Scripts-for-Analysis/KARURU-Analytics/ERPNext-Data-Validation-Scripts/erp-dns.sql
@@ -5,17 +5,18 @@ erp_dns as (
             SELECT *, 
             row_number()over(partition by name order by modified desc) as index 
             FROM `kyosk-prod.erp_reports.delivery_note` 
-            where  territory not in ('Test NG Territory', 'Kyosk TZ HQ', 'Test TZ Territory', 'Kyosk HQ','DKasarani', 'Test KE Territory', 'Test UG Territory')
-            --and date(creation) between '2023-10-01' and '2023-10-29'
+            --where date(creation) >= '2022-02-01'
+            where date(creation) between '2022-02-01' and '2023-11-08'
+            --where  territory not in ('Test NG Territory', 'Kyosk TZ HQ', 'Test TZ Territory', 'Kyosk HQ','DKasarani', 'Test KE Territory', 'Test UG Territory')
+            
             ),
 dns_summary as (
-                select distinct 
+                select distinct date(creation) as creation,
                 dn.territory,
                 customer,
                 dn.name,
-                from erp_dns dn, unnest(items) dni
+                from erp_dns dn
                 where index = 1
-                and against_sales_order in ('SAL-ORD-EMBRGNL', 'SAL-ORD-KISWU5H')
                 )
-select *
+select count(distinct name)
 from dns_summary
