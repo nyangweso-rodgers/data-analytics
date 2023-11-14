f32bc62413f0e274582675d57cf3aca85d874447
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-11-14 21:51:08 +0300

    ranamed outlets script

--- Analytics-in-BigQuery/v2-Scripts-for-Analysis/KARURU-Analytics/Data-Validation-Karuru/karuru-specific-scripts/outlets.sql
+++ Analytics-in-BigQuery/v2-Scripts-for-Analysis/KARURU-Analytics/Data-Validation-Karuru/karuru-specific-scripts/outlets.sql
@@ -7,14 +7,15 @@ karuru_outlets as (
                   FROM `kyosk-prod.karuru_reports.outlets` 
                   WHERE date(created_at) > '2022-01-01'
                   ),
-karuru_outlets_lists as (
-                        SELECT distinct date(created_at) as created_at,
-                        id,
-                        market.market_name as market_name,
-                        FROM karuru_outlets--.market
-                        --left join unnest(market) as market
-                        WHERE index = 1
-                        )
+outlets_lists as (
+                    SELECT distinct date(created_at) as created_at,
+                    id,
+                    name,
+                    market.market_name as market_name,
+                    FROM karuru_outlets--.market
+                    --left join unnest(market) as market
+                    WHERE index = 1
+                    )
 select *
 from karuru_outlets_lists
 where (market_name is null)
