106262cc0e1f6bc02179015948c9e1b19b23f86d
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-11-28 18:58:56 +0300

    added 3 scripts to link dns and si

---
+++ Analytics-in-BigQuery/v2-Scripts-for-Analysis/KARURU-Analytics/Data-Validation-Karuru/karuru-specific-scripts/si-items.sql
@@ -0,0 +1,32 @@
+------------------------- KARURU -----------------
+-------------------- Sales Invoice ------------------------------------
+with
+
+karuru_si as (
+              SELECT *,
+              row_number()over(partition by id order by modified desc) as index
+              FROM `kyosk-prod.karuru_reports.sales_invoice`
+              --where date(created) between '2023-09-01' and  '2023-09-10'
+              WHERE date(created) >= '2022-01-01'
+              --and docstatus = 1
+              and is_karuru_applied = true
+              and territory_id not in ('Test NG Territory', 'Kyosk TZ HQ', 'Test TZ Territory', 'Kyosk HQ','DKasarani', 'Test KE Territory', 'Test UG Territory', 'Test Fresh TZ Territory')
+              ),
+si_summary as (
+                select distinct date(created) as created,
+                si.name,
+                si.po_no,
+                --si.kyosk_delivery_note,
+                --sii.sales_order,
+                sii.delivery_note,
+                grand_total,
+                is_karuru_applied
+                from karuru_si si, unnest(items) sii
+                where index =1
+                --and company_id = 'KYOSK DIGITAL SERVICES LTD (KE)'
+                --and company_id = 'KYOSK DIGITAL SOLUTIONS NIGERIA LIMITED'
+                --and company_id = 'KYOSK DIGITAL SERVICES LIMITED (TZ)'
+                )
+
+select *
+from si_summary
