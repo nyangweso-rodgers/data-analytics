0f713ebb240b375e50a97f9d1311f931e343ef41
Rodgers Nyangweso <rodgerso65@gmail.com>
2024-01-18 12:04:43 +0300

    edited so script for migration

--- Analytics-in-BigQuery/v2-Scripts-for-Analysis/KARURU-Analytics/Data-Validation-Migration/erp-karuru-so-items.sql
+++ Analytics-in-BigQuery/v2-Scripts-for-Analysis/KARURU-Analytics/Data-Validation-Migration/erp-karuru-so-items.sql
@@ -1,53 +1,64 @@
 ------------ ERPNext vs. Karuru 
---------------- Sales Orders Items ---------------------
+--------------- SO Items ---------------------
 with
 erp_so as (
             SELECT *,
             row_number()over(partition by name order by modified desc) as index
             FROM `kyosk-prod.erp_reports.sales_order` 
-            where date(creation) between '2022-02-01' and '2023-10-23'
-            --where date(creation) between '2023-09-01' and '2023-09-30'
-            --where date(creation) = '2023-10-23'
+            where date(creation) between '2022-02-01' and '2023-12-10'
             and territory not in ('Test NG Territory', 'Kyosk TZ HQ', 'Test TZ Territory', 'Kyosk HQ','DKasarani', 'Test KE Territory', 'Test UG Territory')
             ),
-erp_order_items as (
-                    select distinct date(so.creation) as erp_creation,
-                    so.name,
-                    soi.item_code,
-                    soi.item_group,
-                    soi.fulfilment_status
-                    from erp_so so, unnest(items) soi
-                    where index = 1
-                    --and so.name  = 'SAL-ORD-RUIZJFP'
-                    ),
+erp_so_items as (
+                select distinct date(so.creation) as erp_creation,
+                so.name,
+                soi.item_code,
+                soi.item_group,
+                case
+                  when soi.fulfilment_status = 'CANCELLED' then 'ITEM_CANCELLED'
+                  --when soi.fulfilment_status = 'CHANGE_REQUESTED' then 'CHANGE_REQUESTED'
+                  when soi.fulfilment_status = 'COMPLETED' then 'ITEM_FULFILLED'
+                  when soi.fulfilment_status = 'DELIVERED' then 'ITEM_FULFILLED'
+                  when soi.fulfilment_status = 'DELIVERING' then 'DELIVERING'
+                  when soi.fulfilment_status = 'DISPATCHED' then 'ITEM_DISPATCHED'
+                  when soi.fulfilment_status = 'DISPATCHING' then 'DISPATCHING'
+                  when soi.fulfilment_status = 'DRIVER_CANCELLED' then 'ITEM_CANCELLED'
+                  when soi.fulfilment_status = 'DRIVER_RESCHEDULED' then 'ITEM_RESCHEDULED'
+                  when soi.fulfilment_status = 'PAID' then 'ITEM_FULFILLED'
+                  when soi.fulfilment_status = 'RESCHEDULED' then 'ITEM_RESCHEDULED'
+                  when soi.fulfilment_status = 'SOLD_ON_CREDIT' then 'SOLD_ON_CREDIT'
+                  when soi.fulfilment_status = 'SUBMITTED' then 'ITEM_PROCESSING'
+                else soi.fulfilment_status end as fulfilment_status
+                from erp_so so, unnest(items) soi
+                where index = 1
+                and so.name not in (SELECT name FROM `kyosk-prod.erp_reports.so_items_with_multiple_fulfilment_status` )
+                ),
 karuru_so as (
               SELECT *,
               row_number()over(partition by name  order by last_modified_date desc) as index
               FROM `kyosk-prod.karuru_reports.sales_order` so
-              where date(created_date) between '2022-02-01' and '2023-10-23'
-              --WHERE date(created_date) between '2023-09-01' and '2023-09-30'
-              --where date(created_date) = '2023-10-23'
+              where date(created_date) between '2022-02-01' and '2023-12-10'
               and territory_id not in ('Test NG Territory', 'Kyosk TZ HQ', 'Test TZ Territory', 'Kyosk HQ','DKasarani', 'Test KE Territory', 'Test UG Territory')
               and is_pre_karuru = true
               ),
-karuru_order_items as (
-                        select distinct so.name,
-                        soi.product_bundle_id,
-                        soi.category_id,
-                        soi.fulfilment_status
-                        from karuru_so so, unnest(items) as soi
-                        where index = 1
-                        ),
+karuru_so_items as (
+                    select distinct so.name,
+                    so.bq_upload_time,
+                    soi.product_bundle_id,
+                    soi.category_id,
+                    soi.fulfilment_status
+                    from karuru_so so, unnest(items) as soi
+                    where index = 1
+                    ),
 mashup as (
             select e.*,
             k.fulfilment_status as karuru_fulfilment_status,
             e.item_group = k.category_id as check_for_item_group,
-            e.fulfilment_status = k.fulfilment_status as check_for_fulfilment_status
-            from erp_order_items e
-            left join karuru_order_items k on e.name = k.name and e.item_code = k.product_bundle_id
+            e.fulfilment_status = k.fulfilment_status as check_for_fulfilment_status,
+            k.bq_upload_time
+            from erp_so_items e
+            left join karuru_so_items k on e.name = k.name and e.item_code = k.product_bundle_id
             )
-select distinct fulfilment_status, karuru_fulfilment_status
+select *
 from mashup
-where check_for_fulfilment_status is false
---order by erp_creation desc, name, item_code
-order by 1,2
+where (check_for_item_group = false) --or 
+--where (check_for_fulfilment_status = false)
--- Analytics-in-BigQuery/v2-Scripts-for-Analysis/KARURU-Analytics/Data-Validation-Migration/erp-karuru-so.sql
+++ Analytics-in-BigQuery/v2-Scripts-for-Analysis/KARURU-Analytics/Data-Validation-Migration/erp-karuru-so.sql
@@ -5,44 +5,66 @@ erp_so as (
             SELECT *,
             row_number()over(partition by name order by modified desc) as index
             FROM `kyosk-prod.erp_reports.sales_order` 
-            where date(creation) between '2022-02-01' and '2023-11-01'
+            where date(creation) between '2022-02-01' and '2023-12-10'
             and territory not in ('Test NG Territory', 'Kyosk TZ HQ', 'Test TZ Territory', 'Kyosk HQ','DKasarani', 'Test KE Territory', 'Test UG Territory')
             ),
-erp_order_items as (
-                    select distinct so.name,
-                    so.workflow_state,
-                    date(creation) as creation,
-                    --sales_partner
-                    from erp_so so
-                    where index = 1
-                    ),
+erp_so_report as (
+                  select distinct so.name,
+                  case
+                    when so.workflow_state = 'PAID' then 'COMPLETED'
+                    --when so.workflow_state = 'DELIVERED' then 'DELIVERED'
+                    --when so.workflow_state = 'USER_CANCELLED' then 'USER_CANCELLED'
+                    --6when so.workflow_state = 'DISPATCHED' then 'DISPATCHED'
+                    --when so.workflow_state = 'SUBMITTED' then 'SUBMITTED'
+                    --when so.workflow_state = 'COMPLETED' then 'COMPLETED'
+                    --when so.workflow_state = 'INITIATED' then 'INITIATED'
+                    --when so.workflow_state = 'EXPIRED' then 'EXPIRED'
+                    when so.workflow_state = 'DRIVER_CANCELLED' then 'CANCELLED'
+                    when so.workflow_state = 'EXPIRED_PROCESSING' then 'EXPIRED'
+                    --when so.workflow_state = 'PROCESSING' then 'PROCESSING'
+                    --when so.workflow_state = 'DISPATCHING' then 'DISPATCHING'
+                    --when so.workflow_state = 'SOLD_ON_CREDIT' then 'SOLD_ON_CREDIT' 
+                    when so.workflow_state = 'OMS_CANCELLED' then 'CANCELLED'
+                    --when so.workflow_state = 'DELIVERING' then 'DELIVERING'
+                    --when so.workflow_state = 'CANCELLED' then 'CANCELLED'
+                    when so.workflow_state = 'DRIVER_RESCHEDULED' then 'RESCHEDULED'
+                  else so.workflow_state end as workflow_state,
+                  date(creation) as creation,
+                  sales_partner
+                  from erp_so so
+                  where index = 1
+                  --and name in ('SAL-ORD-EMBCXQL')
+                  ),
 karuru_so as (
               SELECT *,
               row_number()over(partition by name  order by last_modified_date desc) as index
               FROM `kyosk-prod.karuru_reports.sales_order` so
-              WHERE date(created_date) between '2022-02-01' and '2023-11-01'
+              WHERE date(created_date) between '2022-02-01' and '2023-12-31'
               and territory_id not in ('Test NG Territory', 'Kyosk TZ HQ', 'Test TZ Territory', 'Kyosk HQ','DKasarani', 'Test KE Territory', 'Test UG Territory')
               and is_pre_karuru = true
               ),
-karuru_order_items as (
-                        select distinct so.name,
-                        date(created_date) as created_date,
-                        so.order_status,
-                        --market_developer_name
-                        from karuru_so so
-                        where index = 1
-                        ),
+karuru_so_report as (
+                      select distinct so.name,
+                      date(created_date) as created_date,
+                      so.order_status,
+                      market_developer_name,
+                      so.bq_upload_time
+                      from karuru_so so
+                      where index = 1
+                      ),
 mashup as (
             select e.*,
             k.created_date,
-            --k.market_developer_name,
+            k.order_status,
+            k.market_developer_name,
             e.creation = k.created_date as check_date,
-            --e.sales_partner = k.market_developer_name as md_check
-            k.order_status
-            from erp_order_items e
-            left join karuru_order_items k on e.name = k.name
+            e.sales_partner = k.market_developer_name as check_md,
+            e.workflow_state = k.order_status as check_status,
+            k.bq_upload_time
+            from erp_so_report e
+            left join karuru_so_report k on e.name = k.name
             )
 select *
 from mashup
-where check_date = false
-order by 1,2
+where (check_date = false) OR (check_status = false)-- or (check_md = false)
+order by creation desc
