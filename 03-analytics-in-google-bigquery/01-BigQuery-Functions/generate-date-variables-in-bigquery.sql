12a0234a2eec0cb09d095cb2325102398f4796fe
nyangweso-rodgers <rodgerso65@gmail.com>
2024-09-06 20:58:50 +0300

    date variables script

---
+++ 03-analytics-in-google-bigquery/01-BigQuery-Functions/generate-date-variables-in-bigquery.sql
@@ -0,0 +1,17 @@
+---------------------- Dynamic Date Variables for SQL Scripts ----------------------
+with 
+dates as (
+          SELECT * FROM  UNNEST(GENERATE_DATE_ARRAY('2020-01-01',date_add(CURRENT_DATE(),interval 31 day), INTERVAL 1 DAY)) AS date
+          ),
+vars AS (
+ --SELECT PARSE_DATE('%Y%m%d', @DS_START_DATE) as current_start_date, PARSE_DATE('%Y%m%d', @DS_END_DATE) end  as current_end_date ),
+  SELECT DATE '2024-08-01' as current_start_date,  DATE '2021-08-31' as current_end_date ),
+
+date_vars as (  
+              select *,
+                date_sub(date_trunc(current_start_date, month), interval 1 month) as previous_start_month,
+                date_sub(date_trunc(current_start_date, month), interval 1 day) as previous_end_month,
+              from vars
+                )
+              
+select * from date_vars
