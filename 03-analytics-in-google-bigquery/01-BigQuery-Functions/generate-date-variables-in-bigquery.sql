ee66d345b7e84b6e624e2d5a3658094304c4ee2c
nyangweso-rodgers <rodgerso65@gmail.com>
2024-10-02 08:06:36 +0300

    date variables in bigquery

--- 03-analytics-in-google-bigquery/01-BigQuery-Functions/generate-date-variables-in-bigquery.sql
+++ 03-analytics-in-google-bigquery/01-BigQuery-Functions/generate-date-variables-in-bigquery.sql
@@ -5,10 +5,12 @@ dates as (
           ),
 vars AS (
  --SELECT PARSE_DATE('%Y%m%d', @DS_START_DATE) as current_start_date, PARSE_DATE('%Y%m%d', @DS_END_DATE) end  as current_end_date ),
-  SELECT DATE '2024-08-01' as current_start_date,  DATE '2021-08-31' as current_end_date ),
+  SELECT DATE '2024-09-10' as current_start_date,  DATE '2021-09-10' as current_end_date ),
 
 date_vars as (  
               select *,
+                date_sub(current_start_date, interval 7 day) as previous_seven_day_start_date,
+                date_sub(current_start_date, interval 1 day) as previous_seven_day_end_date,
                 date_sub(date_trunc(current_start_date, month), interval 1 month) as previous_start_month,
                 date_sub(date_trunc(current_start_date, month), interval 1 day) as previous_end_month,
               from vars
