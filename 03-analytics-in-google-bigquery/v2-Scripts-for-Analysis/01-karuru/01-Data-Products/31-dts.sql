5152700025871157a427ea3212c49782244d3c17
nyangweso-rodgers <rodgerso65@gmail.com>
2024-11-06 16:09:00 +0300

    update bq scripts

--- 03-analytics-in-google-bigquery/v2-Scripts-for-Analysis/01-karuru/01-Data-Products/31-dts.sql
+++ 03-analytics-in-google-bigquery/v2-Scripts-for-Analysis/01-karuru/01-Data-Products/31-dts.sql
@@ -6,19 +6,16 @@ delivery_trips as (
                 row_number()over(partition by id order by updated_at desc) as index
                 FROM `kyosk-prod.karuru_reports.delivery_trips` 
                 where territory_id not in ('Test UG Territory', 'Test NG Territory', 'Kyosk TZ HQ', 'Test TZ Territory', 'Kyosk HQ','DKasarani', 'Test KE Territory', 'Test Fresh TZ Territory')
-                --and date(created_at) > '2024-10-01'
+                and date(created_at) > '2022-01-01'
                 --where date(created_at) = current_date
-                and date_trunc(date(created_at),month) >= date_sub(date_trunc(current_date, month), interval 1 month)
+                --and date_trunc(date(created_at),month) >= date_sub(date_trunc(current_date, month), interval 1 month)
                 --and date(created_at) between '2024-05-01' and '2024-06-31'
                 --and is_pre_karuru = false
-                --and id = '0HH2844KZ38YX'
                 --and code like "DT-KHETIA%"
-                --and id = '0HNXFZ7CF0X0V'
                 --and country_code = 'KE'
-                --and id = '0HQGHVF4MTENB'
-                --and id = '0HQGHVF4MTENB'
                 and territory_id = 'Ruiru'
-                --and id = '0HQGHVF4MTENB'
+                --and id = '0HQTDQ89G1W3T'
+                --and fulfillment_center_id = '0HEHY3146QXKF'
               ),
 dt_status_change_history_cte as (    
                                   select distinct dt.id,
@@ -30,7 +27,7 @@ dt_status_change_history_cte as (
                                   --max(case when sch.to_status = 'DISPATCHED' then sch.change_time end)  as dispatched_time,
                                   --max(case when sch.to_status = 'DELIVERED' then sch.change_time end)  as delivered_time
                                   from delivery_trips dt,unnest(status_change_history) sch
-                                  where index = 1 
+                                  --where index = 1 
                                   --group by 1
                                   ),
 dts_cte as (
@@ -38,7 +35,7 @@ dts_cte as (
             updated_at,
             bq_upload_time,
 
-            dtschct.change_time as dt_completed_datetime,
+            --dtschct.change_time as dt_completed_datetime,
 
             country_code,
             territory_id,
@@ -46,6 +43,8 @@ dts_cte as (
 
             warehouse_location.latitude as warehouse_latitude,
             warehouse_location.latitude as warehouse_longitude,
+
+            --warehouse_location_v2.latitude as warehouse_v2_latitude,
 
             is_preplanned, 
             dispatch_time.dispatch_window_type,
@@ -77,9 +76,11 @@ dts_cte as (
             service_provider.name as service_provider_name,
             
             from delivery_trips dt, unnest(delivery_note_ids) delivery_note_ids
-            left join (select distinct id, change_time from dt_status_change_history_cte where to_status = 'COMPLETED') dtschct on dt.id = dtschct.id
+            --left join (select distinct id, change_time from dt_status_change_history_cte where to_status = 'COMPLETED') dtschct on dt.id = dtschct.id
             where index = 1
           )
-select * from dts_cte
+--select distinct created_at, updated_at, bq_upload_time,id, code, status from dts_cte
 --distinct country_code, count(distinct id)
+--select min(created_at) as min_created_at_datetime from dts_cte
 --select max(created_at) as max_created_at_datetime, max(updated_at) as max_updated_at_datetime, max(bq_upload_time) as max_bq_upload_time_datetime from dts_cte
+select distinct territory_id, id, status from dts_cte
