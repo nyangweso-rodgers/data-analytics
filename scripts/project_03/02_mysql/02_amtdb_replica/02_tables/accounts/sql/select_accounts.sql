4ec1f4ce041b7a23d2fd5f0b5da577531760c033
nyangweso-rodgers <rodgerso65@gmail.com>
2026-03-28 10:18:15 +0300

    add accounts query

---
+++ scripts/project_03/02_mysql/02_amtdb_replica/02_tables/accounts/sql/select_accounts.sql
@@ -0,0 +1,35 @@
+with
+accounts_cte as (
+	SELECT id, 
+	#old_id, 
+	accountTypeId, 
+	#old_payplan, old_account_type_id, 
+	customerId, 
+	#old_customer_id, paygContractNumber, 
+accountRef, 
+#acreage, accountBypass, accountNotes, 
+status, accountBalance, 
+#fvreceivable, jsfDate, jsfId, parentAccountId, 
+#dispatchDate, expectedStartDate, 
+#firstInstallmentDate, isRevenuePosted, revenuePostedAt, manualDate, revenueReversalAt, externalId, installationId, installationDate, depositPaymentId, fullDepositDate, externalIdSource, 
+createdAt, 
+#createdBy, old_created_by, updatedAt, updatedBy, old_updated_by, 
+#salesAgents, assignmentId, assignmentDate, netSuiteAccountId, isMigrated, isWalletActive, walletID, 
+creditCheck, creditCheckId
+FROM amtdb.accounts
+),
+validate_credit_check_cte as (
+	select distinct creditCheck,
+	count(*)
+	from accounts_cte
+	GROUP BY 1
+	ORDER BY 2 DESC
+	)
+select *
+#count(*)
+#distinct creditCheck
+#from accounts_cte
+from validate_credit_check_cte
+#where creditCheck is not null
+#where id in ('154109')
+limit 1000
