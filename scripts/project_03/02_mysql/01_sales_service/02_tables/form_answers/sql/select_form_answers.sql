6c8715123c5c22421f6d66ae819637187fd327bb
nyangweso-rodgers <rodgerso65@gmail.com>
2026-04-07 21:58:07 +0300

    add form answers script

--- scripts/project_03/02_mysql/01_sales_service/02_tables/form_answers/sql/select_form_answers.sql
+++ scripts/project_03/02_mysql/01_sales_service/02_tables/form_answers/sql/select_form_answers.sql
@@ -1,9 +1,11 @@
 with
 form_answers_cte as (
-	SELECT id, leadId, questionId, cdsId, answer, createdBy, updatedBy, createdAt, updatedAt, answerSetId, 
-	#questionTemplateId, questionInstanceId, answerType, valuePayload, isNull, 
-	recordedAt 
-	#expectedAnswerId
+	SELECT id, leadId, questionId, cdsId, answer, 
+	createdBy, updatedBy, createdAt, updatedAt, 
+	answerSetId, 
+	questionTemplateId, questionInstanceId, answerType, valuePayload, isNull, 
+	recordedAt, 
+	expectedAnswerId
 	FROM `sales-service`.form_answers
 	),
 check_duplicates_cte as (
@@ -17,7 +19,11 @@ check_duplicates_cte as (
 	order by 2 desc
 	)
 select *
+#count(*), count(distinct cdsId) as cds_id_count
+#distinct answer, count(distinct cdsId)
 from form_answers_cte
-#where cdsId = '8141'
-where cdsId is null
-limit 100
+#where questionId ='did_you_previously_own_a_water_pump_c'
+where cdsId = '8141'
+#where cdsId is null
+#group by 1 order by 2 desc
+#limit 100
