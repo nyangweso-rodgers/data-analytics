6a573cd944c91cfe62f162047379b7f797754072
nyangweso-rodgers <rodgerso65@gmail.com>
2026-04-10 07:17:40 +0300

    ++ sql script

--- scripts/project_03/02_mysql/02_amtdb_replica/02_tables/accounts/sql/select_accounts.sql
+++ scripts/project_03/02_mysql/02_amtdb_replica/02_tables/accounts/sql/select_accounts.sql
@@ -6,30 +6,30 @@ accounts_cte as (
 	#old_payplan, old_account_type_id, 
 	customerId, 
 	#old_customer_id, paygContractNumber, 
-accountRef, 
-#acreage, accountBypass, accountNotes, 
-status, accountBalance, 
-#fvreceivable, jsfDate, jsfId, parentAccountId, 
-#dispatchDate, expectedStartDate, 
-#firstInstallmentDate, isRevenuePosted, revenuePostedAt, manualDate, revenueReversalAt, externalId, installationId, installationDate, depositPaymentId, fullDepositDate, externalIdSource, 
-createdAt, 
-#createdBy, old_created_by, updatedAt, updatedBy, old_updated_by, 
-#salesAgents, assignmentId, assignmentDate, netSuiteAccountId, isMigrated, isWalletActive, walletID, 
-creditCheck, creditCheckId
-FROM amtdb.accounts
+	accountRef, 
+	#acreage, accountBypass, accountNotes, 
+	status, 
+	#accountBalance, fvreceivable, jsfDate, jsfId, parentAccountId, dispatchDate, expectedStartDate, 
+	#firstInstallmentDate, isRevenuePosted, revenuePostedAt, manualDate, revenueReversalAt, externalId, installationId, installationDate, depositPaymentId, fullDepositDate, externalIdSource, 
+	createdAt, 
+	#createdBy, old_created_by, updatedAt, updatedBy, old_updated_by, 
+	#salesAgents, assignmentId, assignmentDate, netSuiteAccountId, isMigrated, isWalletActive, walletID, 
+	creditCheck, creditCheckId
+	FROM amtdb.accounts
 ),
 validate_credit_check_cte as (
-	select distinct creditCheck,
-	count(*)
+	select distinct customerId,
+	id as accountId,
+	creditCheck
+	#count(*)
 	from accounts_cte
-	GROUP BY 1
-	ORDER BY 2 DESC
+	#GROUP BY 1
+	#ORDER BY 2 DESC
 	)
 select *
 #count(*)
 #distinct creditCheck
-#from accounts_cte
-from validate_credit_check_cte
+from accounts_cte
+#from validate_credit_check_cte
 #where creditCheck is not null
-#where id in ('154109')
-limit 1000
+where id in ()
