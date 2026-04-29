7a046fc49479a34e9faea14658176f7e74a09f7c
nyangweso-rodgers <rodgerso65@gmail.com>
2026-04-29 08:53:23 +0300

    ++ list stored proc

--- scripts/project_03/02_mysql/02_amtdb_replica/01_meta/stored_procedure/sql/list_amtdb_stored_proc.sql
+++ scripts/project_03/02_mysql/02_amtdb_replica/01_meta/stored_procedure/sql/list_amtdb_stored_proc.sql
@@ -1,1 +1,25 @@
+# List all stored procedures in a database:
 SHOW PROCEDURE STATUS WHERE Db = 'amtdb';
+
+# See all procedures you have access to:
+SHOW PROCEDURE STATUS;
+
+# View the stored procedure code:
+SHOW CREATE PROCEDURE procedure_name;
+
+# Query the information schema:
+SELECT * FROM information_schema.ROUTINES 
+WHERE ROUTINE_TYPE = 'PROCEDURE' 
+AND ROUTINE_SCHEMA = 'your_database_name';
+
+# For a specific procedure:
+SELECT * FROM information_schema.ROUTINES 
+WHERE ROUTINE_TYPE = 'PROCEDURE' 
+AND ROUTINE_NAME = 'procedure_name'
+AND ROUTINE_SCHEMA = 'your_database_name';
+
+# Get just the procedure definition:
+SELECT ROUTINE_DEFINITION 
+FROM information_schema.ROUTINES 
+WHERE ROUTINE_NAME = 'procedure_name' 
+AND ROUTINE_SCHEMA = 'your_database_name';
