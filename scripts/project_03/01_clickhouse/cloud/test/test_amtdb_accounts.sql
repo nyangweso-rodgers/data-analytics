0cb6920b7aaa834382b2c33d84b462c34b705158
nyangweso-rodgers <rodgerso65@gmail.com>
2026-05-03 22:46:15 +0300

    add test clickhouse db

---
+++ scripts/project_03/01_clickhouse/cloud/test/test_amtdb_accounts.sql
@@ -0,0 +1,4 @@
+SELECT count(*) 
+FROM test.test_amtdb_accounts
+--where accountId = '143073'
+LIMIT 100
---
+++ scripts/project_03/01_clickhouse/cloud/test/test_amtdb_installment_schedules.sql
@@ -0,0 +1,4 @@
+SELECT count(*) 
+FROM test.test_amtdb_installment_schedules 
+--where accountId = '143073'
+LIMIT 100
