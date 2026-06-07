c676781c1a659bb6c52976c335304af000257d11
nyangweso-rodgers <rodgerso65@gmail.com>
2026-06-07 20:30:31 +0300

    ++ query

---
+++ scripts/project_03/01_clickhouse/cloud/adx/02_tables/device_daily_usage/sql/alter_device_daily_usage.sql
@@ -0,0 +1,2 @@
+ALTER TABLE adx.device_daily_usage
+ADD COLUMN _sync_at Nullable(DateTime) DEFAULT NULL
--- scripts/project_03/01_clickhouse/cloud/adx/02_tables/device_daily_usage/sql/select_device_daily_usage.sql
+++ scripts/project_03/01_clickhouse/cloud/adx/02_tables/device_daily_usage/sql/select_device_daily_usage.sql
@@ -1,8 +1,34 @@
 with
+--------------------- ADX - Device Daily Usage ----------------------------------
 device_daily_usage_cte as (
-    SELECT * 
+    SELECT distinct deviceId,
+    timestamp,
+    energyConsumptionKwh,
+    timeIntervalMinutes,
+    Source,
+    FwVer,
+    Variant
     FROM adx.device_daily_usage
-    )
+    ),
+--------------------- Validate Duplicates ----------------------------------
+validate_duplicate_cte as (
+    select distinct timestamp,
+    deviceId,
+    count(*) as record_count
+    from device_daily_usage_cte
+    GROUP BY 1,2
+    HAVING record_count > 1
+)
 select *
+--count(*)
+--min(timestamp) as min_timestamp, max(timestamp) as max_timestamp, count(*), count( distinct deviceId)
+--min(energyConsumptionKwh)
 from device_daily_usage_cte
-LIMIT 31 OFFSET 0;
+--from validate_duplicate_cte
+--where timestamp = '2026-04-27'
+--where deviceId = '868328054241050'
+--where timeIntervalMinutes = 0
+where energyConsumptionKwh = 0
+--ORDER BY deviceId, timestamp
+ORDER BY timestamp desc
+LIMIT 1000;
