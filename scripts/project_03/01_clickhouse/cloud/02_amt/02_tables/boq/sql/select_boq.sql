7fab9ae84bc9a285a25696f5d4e7433ab07fbb41
nyangweso-rodgers <rodgerso65@gmail.com>
2026-06-28 16:22:13 +0300

    ++ query

--- scripts/project_03/01_clickhouse/cloud/02_amt/02_tables/boq/sql/select_boq.sql
+++ scripts/project_03/01_clickhouse/cloud/02_amt/02_tables/boq/sql/select_boq.sql
@@ -1,15 +1,17 @@
 WITH
 --------------------- BOQ ----------------------------------
 boq_cte as (
-    SELECT id,
-    accountId,
-    boqStatus,
-    boqDate,
-    row_number() OVER (partition by id ORDER BY updatedAt DESC) as rnk  
-    FROM amt.boq
+    select *
+    from (
+        SELECT id,
+        accountId,
+        boqStatus,
+        boqDate,
+        row_number() OVER (partition by id ORDER BY updatedAt DESC) as rnk  
+        FROM amt.boq
+    ) WHERE rnk = 1
     )
 select --*
 distinct boqStatus
 from boq_cte
 limit 10
-
