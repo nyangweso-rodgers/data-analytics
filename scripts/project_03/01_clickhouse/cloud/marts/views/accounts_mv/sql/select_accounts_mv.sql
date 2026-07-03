17ecfdbbaca235ca098a976bb127812d3eb0bef7
nyangweso-rodgers <rodgerso65@gmail.com>
2026-07-03 07:43:55 +0300

    update query

--- scripts/project_03/01_clickhouse/cloud/marts/views/accounts_mv/sql/select_accounts_mv.sql
+++ scripts/project_03/01_clickhouse/cloud/marts/views/accounts_mv/sql/select_accounts_mv.sql
@@ -1,8 +1,22 @@
 with
+--------------------- View - accounts_mv ----------------------------------
 accounts_mv_cte as (
     select *
     from marts.accounts_mv
 ),
+--------------------- Agg - Account Status ----------------------------------
+agg_account_status_cte as (
+    select distinct 
+    --status,
+    region,
+    County,
+    count(distinct account_id) as account_id_count
+    from accounts_mv_cte
+    where companyRegion = 'kenya'
+    group by 1,2
+    ORDER BY 1,2
+),
+--------------------- Agg - Daily Sales ----------------------------------
 agg_daily_sales_cte as (
     SELECT distinct companyRegion,
     date(sale_date) as sale_date,
@@ -16,6 +30,7 @@ agg_daily_sales_cte as (
     )
 select *
 --from accounts_mv_cte
-from agg_daily_sales_cte
+--from agg_daily_sales_cte
+from agg_account_status_cte
 --where account_id = '100228'
 LIMIT 1000
