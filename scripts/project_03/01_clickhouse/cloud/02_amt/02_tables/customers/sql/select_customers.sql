9dbe39bf2bf43ac1feb7df10e16f055b67ceb3a6
nyangweso-rodgers <rodgerso65@gmail.com>
2026-07-16 08:56:22 +0300

    ++ query

--- scripts/project_03/01_clickhouse/cloud/02_amt/02_tables/customers/sql/select_customers.sql
+++ scripts/project_03/01_clickhouse/cloud/02_amt/02_tables/customers/sql/select_customers.sql
@@ -1,10 +1,10 @@
 WITH
+--------------------- Customers ----------------------------------
 customers_cte as (
     SELECT *
     from (
         select *,
         row_number() OVER (partition by id ORDER BY updatedAt DESC) as rnk 
-        --FROM sunculture.customers
         FROM amt.customers
         ) where rnk = 1 
     )
