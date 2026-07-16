92c50e3e7b89afbbd6fc3d02ab8b6ed1189aa462
nyangweso-rodgers <rodgerso65@gmail.com>
2026-07-16 08:56:45 +0300

    ++ query

--- scripts/project_03/01_clickhouse/cloud/02_amt/02_tables/ledger_entries/sql/create_ledger_entries_snapshot.sql
+++ scripts/project_03/01_clickhouse/cloud/02_amt/02_tables/ledger_entries/sql/create_ledger_entries_snapshot.sql
@@ -1,4 +1,4 @@
-CREATE TABLE snapshots.ledger_entries_2026_04_26
+CREATE TABLE snapshots.ledger_entries_2026_04_30
 ENGINE = MergeTree()
 ORDER BY (account_id, id)
 AS SELECT * FROM amt.ledger_entries
