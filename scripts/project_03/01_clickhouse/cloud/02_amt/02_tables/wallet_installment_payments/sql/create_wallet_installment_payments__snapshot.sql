ae303cc12fdfeec4ac48e5d2b0569f42263df11f
nyangweso-rodgers <rodgerso65@gmail.com>
2026-07-16 08:58:33 +0300

    ++ query

--- scripts/project_03/01_clickhouse/cloud/02_amt/02_tables/wallet_installment_payments/sql/create_wallet_installment_payments__snapshot.sql
+++ scripts/project_03/01_clickhouse/cloud/02_amt/02_tables/wallet_installment_payments/sql/create_wallet_installment_payments__snapshot.sql
@@ -1,4 +1,4 @@
-CREATE TABLE snapshots.wallet_installment_payments_2026_04_26
+CREATE TABLE snapshots.wallet_installment_payments_2026_05_08
 ENGINE = MergeTree()
 ORDER BY (accountId, id)
 AS SELECT * FROM amt.wallet_installment_payments
