ba6a40bf2a60b17bdd56a94531d0455caf43f4a1
nyangweso-rodgers <rodgerso65@gmail.com>
2026-07-16 08:56:55 +0300

    ++ query

--- scripts/project_03/01_clickhouse/cloud/02_amt/02_tables/payments/sql/create_payments_snapshots.sql
+++ scripts/project_03/01_clickhouse/cloud/02_amt/02_tables/payments/sql/create_payments_snapshots.sql
@@ -1,4 +1,4 @@
-CREATE TABLE snapshots.payments_2025_03_08
+CREATE TABLE snapshots.payments_2025_05_03
 ENGINE = MergeTree()
-ORDER BY (id)  -- Replace 'id' with your actual primary key column
-AS SELECT * FROM snapshot.payments_2025_03_08
+ORDER BY (id)  -- Replace 'id' with actual primary key column
+AS SELECT * FROM amt.payments
