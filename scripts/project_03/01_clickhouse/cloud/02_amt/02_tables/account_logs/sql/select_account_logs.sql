a9a062247fb4ce7f8755f181fdf05bd47263b52e
nyangweso-rodgers <rodgerso65@gmail.com>
2026-07-16 08:55:11 +0300

    +++ query

--- scripts/project_03/01_clickhouse/cloud/02_amt/02_tables/account_logs/sql/select_account_logs.sql
+++ scripts/project_03/01_clickhouse/cloud/02_amt/02_tables/account_logs/sql/select_account_logs.sql
@@ -2,12 +2,25 @@ WITH
 account_logs_cte as (
     select *
     from (
-        SELECT *,
-    row_number() OVER (PARTITION BY id ORDER BY updatedAt DESC) AS rnk 
-    FROM amt.account_logs 
-    ) where rnk =1
-    )
+    SELECT distinct accountId,
+    createdAt,
+    updatedAt,
+    eventType,
+    payload,
+    note,
+    createdBy
+    --row_number() OVER (PARTITION BY id ORDER BY updatedAt DESC) AS rnk 
+    FROM amt.account_logs FINAL
+    ) --where rnk = 1
+)
 select *
+--count(*)
 from account_logs_cte
-where toStartOfMonth(createdAt) >= '2026-03-01'
-LIMIT 31
+--where accountId in ('77667', '144635', '168646')
+--where accountId ='176745'
+--WHERE COALESCE(accountId, 0) = '176745'  -- ← Match the ORDER BY expression
+WHERE COALESCE(accountId, 0) ='102933'
+--where date(updatedAt) > '2026-01-01'
+--and eventType not in ('sms')
+ORDER BY accountId, updatedAt
+limit 1000
