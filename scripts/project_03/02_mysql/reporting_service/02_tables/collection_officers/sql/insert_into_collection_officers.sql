beebdd77ef768fa9acf464b57aa0451473726f31
nyangweso-rodgers <rodgerso65@gmail.com>
2026-09-11 10:29:29 +0300

    ++ query

---
+++ scripts/project_03/02_mysql/reporting_service/02_tables/collection_officers/sql/insert_into_collection_officers.sql
@@ -0,0 +1,4 @@
+INSERT INTO collection_officers
+    (employee_id, employee_name, active, primary_role, created_at, created_by, updated_by)
+VALUES
+    (16332, 'Benson Muguku', 1, 'Credit Collections Associate (CCA)', NOW(), 'Rodgers Nyangweso', 'Rodgers Nyangweso');
---
+++ scripts/project_03/02_mysql/reporting_service/02_tables/collection_officers/sql/update_collection_officers.sql
@@ -0,0 +1,4 @@
+UPDATE collection_officers
+SET active = 0,
+    updated_by = 'Rodgers Nyangweso'
+WHERE employee_id = 6644;
